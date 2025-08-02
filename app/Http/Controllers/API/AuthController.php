effa8b9ebb4c0f2a23b6c2eb62f85edf638c677d
SyMihad <symihad@gmail.com>
2025-08-02 21:17:14 +0600

    Login and Registration controller and route added

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['token' => $user->createToken('auth_token')->plainTextToken]);
+    }
+
+    public function login(Request $request) {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages(['email' => ['The provided credentials are incorrect.']]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken('auth_token')->plainTextToken,
+            'is_admin' => $user->is_admin
+        ]);
+    }
+}
